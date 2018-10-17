0230fe9f83bb174a5ab383f6d2696488ba9e87fa
Aaron Goodfellow <amgoodfellow@oakland.edu>
2018-10-17 15:24:54 -0400

    Initial project commit #1

---
+++ src/main/java/com/softwareengineering/forumn/ForumnApplication.java
@@ -0,0 +1,12 @@
+package com.softwareengineering.forumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumnApplication.class, args);
+	}
+}
