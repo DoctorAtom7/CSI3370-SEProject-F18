888827075b58cdf41fd13959731f602374d8661b
ImGonnaPuke <ahorle@oakland.edu>
2018-10-26 17:10:04 -0400

    added file uploader, original plan to have images displayed scrapped, this will however allow users to upload any and all files instead of just images, can later be changed to store images to some SQL database instead of on disk

--- src/main/java/com/softwareengineering/forumn/ForumnApplication.java
+++ src/main/java/com/softwareengineering/forumn/ForumnApplication.java
@@ -1,12 +1,17 @@
 package com.softwareengineering.forumn;
 
+import com.softwareengineering.forumn.controllers.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import java.io.File;
 
 @SpringBootApplication
+@ComponentScan({"com.softwareengineering.forumn","controllers"})
 public class ForumnApplication {
 
 	public static void main(String[] args) {
+		new File(HomeController.upDir).mkdir();
 		SpringApplication.run(ForumnApplication.class, args);
 	}
 }
