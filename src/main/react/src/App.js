b9361b796d6d0f677c436607b15f58f664a03784
Aaron Goodfellow <amgoodfellow@oakland.edu>
2018-10-27 21:36:01 -0400

    Basic frontpage and account creation modal #5

--- src/main/react/src/App.js
+++ src/main/react/src/App.js
@@ -1,28 +1,146 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import Snack from './components/Snack'
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import CreateAccount from './components/CreateAccount'
 
-class App extends Component {
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  appbar: {
+    justifyContent: 'space-between'
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: '50%',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+});
+
+
+class HomePage extends Component {
+
+  state = { logged_in: false, theme: 'light', modal: 'none', loading: true, cookies: true }
+
+  handleClose = (name) => {
+    this.setState({ [name]: false })
+  }
+
+  modalClose = () => {
+    this.setState({ modal: 'none' })
+  }
+
   render() {
+    const { classes } = this.props;
+    const { modal } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar className={classes.appbar}>
+            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                <MenuIcon />
+              </IconButton>
+              <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                Placeholder
+            </Typography>
+
+            </div>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+            <div className={classes.sectionDesktop}>
+              <Button style={{ marginRight: '20px', color: 'white' }} onClick={() => this.setState({ modal: 'create' })} variant="outlined">Login</Button>
+              <Button variant="contained" color="secondary">Sign Up</Button>
+            </div>
+            <div className={classes.sectionMobile}>
+              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <CreateAccount open={modal === 'create'} onClose={this.modalClose} />
+        {Snack(this.state.cookies, "This site uses cookies to provide you a better browsing experience",
+          "OK", () => this.handleClose("cookies"))}
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(HomePage);
