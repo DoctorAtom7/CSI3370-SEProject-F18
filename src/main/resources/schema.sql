c472bea75c3efac625b412026d12c9207aa936ac
Aaron Goodfellow <amgoodfellow@oakland.edu>
2018-11-06 18:47:05 -0500

    Allows front and back end to communicate. Temporarily simplifies passwords until authentication is standardized #4

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,12 +3,7 @@ CREATE TABLE IF NOT EXISTS member(
 	username VARCHAR(50) UNIQUE NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	verified BOOLEAN DEFAULT 'n',
-	create_date TIMESTAMP DEFAULT now()
+	create_date TIMESTAMP DEFAULT now(),
+	password_hash VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS password (
-	member_id REFERENCES member(id) ON DELETE CASCADE,
-	password VARCHAR(50) NOT NULL,
-	salt VARCHAR(50) NOT NULL,
-	change_date TIMESTAMP DEFAULT NULL
-);
