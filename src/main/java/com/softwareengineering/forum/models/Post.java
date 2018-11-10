24ebe0ca88b810e33fb9f311456d34ff8115b98c
Aaron Goodfellow <amgoodfellow@oakland.edu>
2018-11-10 13:39:22 -0500

    Small name changes to Post - username changed to foreign key

--- src/main/java/com/softwareengineering/forum/models/Post.java
+++ src/main/java/com/softwareengineering/forum/models/Post.java
@@ -5,18 +5,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
-
-
 
 //requirements:
 // 1.int to hold post num
@@ -26,63 +19,81 @@ import javax.persistence.Table;
 //5. user that posted
 
 @Entity
-@Table(name="post")
+@Table(name = "post")
 public class Post {
     @Id
     @GeneratedValue
 
-    @Column(name = "postID", nullable = false, unique = true)
+    @Column(name = "post_id", nullable = false, unique = true)
     private int postNum;
 
-    @Column(name = "body", nullable = false)
+    @Column(name = "body")
     private String textBody;
 
-    @Column(name = "topic", nullable = false)
-    private String textTopic;
+    @Column(name = "title")
+    private String title;
 
-    @Column(name = "date", nullable = false)
+    @Column(name = "creation_date", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
 
-    @Column(name = "username", nullable = false, unique = true)
-    private String username;
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Member member;
 
-    public Post(){
-
-    }
-
-    public Post(int postNum, String textBody, String textTopic, Date creationDate, String username){
-
-        this.postNum=postNum;
-        this.textBody=textBody;
-        this.textTopic=textTopic;
-        this.creationDate=creationDate;
-        this.username=username;
+    public Post() {
 
     }
-        //postNum
+
+    public Post(String textBody, String textTopic, Member creator) {
+        this.textBody = textBody;
+        this.title = textTopic;
+        this.member = creator;
+    }
+
+    // postNum
     public int getNum() {
         return postNum;
     }
+
     public void setNum(int postNum) {
         this.postNum = postNum;
     }
-        //textBody
+
+    // textBody
     public String getBody() {
         return textBody;
     }
-    public void setBody(String textBody) { this.textBody = textBody; }
-        //Topic
-    public String getTopic() { return textTopic; }
-    public void setTopic(String textTopic) { this.textTopic = textTopic; }
-        //date
-    public Date getDate(){ return creationDate;}
-    public void setDate(Date creationDate) { this.creationDate = creationDate;}
-        //user
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) { this.username = username; }
 
+    public void setBody(String textBody) {
+        this.textBody = textBody;
+    }
+
+    // Topic
+    public String getTopic() {
+        return title;
+    }
+
+    public void setTopic(String textTopic) {
+        this.title = textTopic;
+    }
+
+    // date
+    public Date getDate() {
+        return creationDate;
+    }
+
+    public void setDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    // user
+    public Member getCreator() {
+        return member;
+    }
+
+    public void setUsername(Member member) {
+        this.member = member;
+    }
 
 }
